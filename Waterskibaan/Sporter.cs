1e54942b406335cfd4b9fe24911c049a34005578
Casper Bloemendaal <casperbloemendaal@gmail.com>
2019-09-17 11:20:59 +0200

    Opzet

---
+++ Waterskibaan/Sporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Sporter : IMoves
+    {
+        public int AantalRondenNogTeGaan { get; set; } = 1;
+        public Zwemvest Zwemvest { get; set; }
+        public Skies Skies { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> Moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+
+        }
+
+        public int Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
