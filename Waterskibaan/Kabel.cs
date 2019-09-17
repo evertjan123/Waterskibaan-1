197280cc1c68f5778d243bfbbc51733290035f65
Casper Bloemendaal <casperbloemendaal@gmail.com>
2019-09-17 12:17:45 +0200

    Opzet Opdracht 2

---
+++ Waterskibaan/Kabel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Kabel
+    {
+        private LinkedList<Lijn> _lijnen;
+
+        public bool IsStartPositieLeeg()
+        {
+            return _lijnen.First == null;
+        }
+        public void NeemLijnInGebruik(Lijn lijn)
+        {
+            if (IsStartPositieLeeg())
+            {
+                _lijnen.AddFirst(lijn);
+            }
+        }
+        public void VerschuifLijnen()
+        {
+            
+        }
+        public Lijn VerwijderLijnVanKabel()
+        {
+            Lijn l = _lijnen.ElementAt<Lijn>(9);
+
+            if (l != null)
+            {
+                _lijnen.Remove(l);
+            }
+            
+            return l;
+        }
+        public override string ToString()
+        {
+            List<int> integers = new List<int>();
+            foreach (var item in _lijnen)
+            {
+                if (item != null)
+                {
+                    integers.Add(item.PositieOpDeKabel);
+                }
+            }
+
+            // return integers.Join<int>("|");
+            return base.ToString();
+        }
+    }
+}
