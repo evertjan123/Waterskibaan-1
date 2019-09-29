2ade866b98ad68165ae53489d659cb0395269e78
Bloemendaal <casperbloemendaal@gmail.com>
2019-09-29 22:07:20 +0200

    Opdracht 4

---
+++ Waterskibaan/Waterskibaan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Waterskibaan
+    {
+        public LijnenVoorraad LijnenVoorraad;
+        public Kabel Kabel;
+        public Waterskibaan()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                LijnenVoorraad.LijnToevoegenAanRij(new Lijn());
+            }
+        }
+        public void VerplaatsKabel()
+        {
+            Kabel.VerschuifLijnen();
+            LijnenVoorraad.LijnToevoegenAanRij(Kabel.VerwijderLijnVanKabel());
+        }
+
+    }
+}
